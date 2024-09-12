9c27cb22d81e7f2d26902acf66c282c3521ee456
Michal Moscovich <michal.mos262@gmail.com>
2024-09-12 14:49:21 +0300

    created commands fxml

--- UI/src/ui/impl/graphic/components/command/CommandsController.java
+++ UI/src/ui/impl/graphic/components/command/CommandsController.java
@@ -4,13 +4,27 @@ import engine.api.Engine;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Spinner;
 import ui.impl.graphic.components.app.MainAppController;
 
 public class CommandsController {
 
-    @FXML private Button filterSheetByColumnButton;
-    @FXML private Button setSheetPropertiesButton;
-    @FXML private Button sortSheetByColumnButton;
+    @FXML private ColorPicker cellBackgroundColorPicker;
+    @FXML private ChoiceBox<?> cellPositionChoiceBox;
+    @FXML private ColorPicker cellTextColorPicker;
+    @FXML private ChoiceBox<?> columnChoiceBox;
+    @FXML private ChoiceBox<?> columnTextAlignChoiceBox;
+    @FXML private Button defaultCellPropsButton;
+    @FXML private Button defaultColumnPropsButton;
+    @FXML private Button defaultRowPropsButton;
+    @FXML private Spinner<?> heightSpinner;
+    @FXML private ChoiceBox<?> rowChoiceBox;
+    @FXML private Button setCellPropsButton;
+    @FXML private Button setColumnPropsButton;
+    @FXML private Button setRowPropsButton;
+    @FXML private Spinner<?> widthSpinner;
 
     private MainAppController mainAppController;
     private Engine engine;
@@ -20,24 +34,37 @@ public class CommandsController {
         this.engine = engine;
     }
 
+    @FXML
+    void defaultCellPropsButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void defaultColumnPropsButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void defaultRowPropsButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setCellPropsButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setColumnPropsButtonListener(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setRowPropsButtonListener(ActionEvent event) {
+
+    }
+
     public void fileLoaded() {
-        filterSheetByColumnButton.disableProperty().set(false);
-        setSheetPropertiesButton.disableProperty().set(false);
-        sortSheetByColumnButton.disableProperty().set(false);
-    }
-
-    @FXML
-    void filterSheetByColumnButtonListener(ActionEvent event) {
-
-    }
-
-    @FXML
-    void setSheetPropertiesButtonListener(ActionEvent event) {
-
-    }
-
-    @FXML
-    void sortSheetByColumnButtonListener(ActionEvent event) {
 
     }
 }
